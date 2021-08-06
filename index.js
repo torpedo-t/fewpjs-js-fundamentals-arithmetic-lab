7b2e289eb5ff6690324dc5291f73183acc171cb5
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-08-06 12:52:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const num1 = 31;
 const num2 = 2;
-// multiply variables num1 and numb2 whols results will equal "62"
-let multiply = num1 * num2;
+const num3 = 16
+const num4 = 6
+// // multiply variables num1 and numb2 whols results will equal "62"
+const multiply = num1 * num2;
 
-// generate a random integer > 0
-let random = Math.floor(Math.random() * 10) + 1;
+// // generate a random integer > 0
+const random = Math.floor(Math.random() * 10) + 1;
 
-// set to an equation whose remainder is equal to "4"
-let mod = 16 %= 6;
+// // set to an equation whose remainder is equal to "4"
+const mod = num3 % num4;
 
-// return "20" as the highest number in the set
-let max = Math.max(1, 5, 10, 15, 20);
+// // return "20" as the highest number in the set
+const max = Math.max(1, 5, 10, 15, 20);
 
 // Write your code here
